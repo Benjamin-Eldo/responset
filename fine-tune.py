b0a1eeafc630ac8766768b4da7355f803d3d7011
Benjamin Vadurel <84413803+Benjamin-Eldo@users.noreply.github.com>
2025-01-18 10:10:20 +0100

    Removed code from the old project

--- fine-tune.py
+++
@@ -1,9 +0,0 @@
-# Load model directly
-from transformers import AutoProcessor, AutoModelForImageTextToText
-
-processor = AutoProcessor.from_pretrained("llava-hf/llava-v1.6-mistral-7b-hf")
-model = AutoModelForImageTextToText.from_pretrained("llava-hf/llava-v1.6-mistral-7b-hf")
-
-
-print('salut')
-print(model)
