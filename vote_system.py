b2983e2eced9aa6162bd6a25f49aa4f0fa3e2009
Benjamin Vadurel <84413803+Benjamin-Eldo@users.noreply.github.com>
2025-01-25 12:18:10 +0100

    vote and tournament

---
+++ vote_system.py
@@ -0,0 +1,24 @@
+from typing import List, Tuple
+from rouge_score import rouge_scorer
+
+
+def confront_dataset(code_list_1 : list, code_list_2 : list) -> List[Tuple[str, str, int]]:
+    """
+    This function receives two lists of strings, each one containing a set of code snippets.
+    It returns a list of tuples, where each tuple contains two code snippets and the similarity score between them.
+    """
+    tuple_with_score = []
+    scorer = rouge_scorer.RougeScorer(['rouge1', 'rougeL'], use_stemmer=True)    
+
+    for code_1 in code_list_1:
+        for code_2 in code_list_2:
+            scores = scorer.score(code_1, code_2)
+            tuple_with_score.append((code_1, code_2, scores['rougeL'].fmeasure))
+    return tuple_with_score
+
+def filter_pairs(tuple_list : List[Tuple[str, str, int]], threshold : float = 0.7) -> List[Tuple[str, str, int]]:
+    """
+    This function receives a list of tuples, each one containing two code snippets and the similarity score between them.
+    It returns a list of tuples, where each tuple contains two code snippets and the similarity score between them.
+    """
+    return [(code_1, code_2, score) for code_1, code_2, score in tuple_list if score >= threshold]
