77029a611cf28869283d5a0ff1373eb13f00eaf3
Desbiaux Arthur <arthur.desbiaux@etu.univ-lyon1.fr>
2025-01-25 13:17:40 +0100

    Try to parse qwen

--- utils.py
+++ utils.py
@@ -1,4 +1,5 @@
 import json
+import re
 
 def parse_gemma(dataset : str, website_id : str) -> list[str]:
     parsed_responsive_line = []
@@ -21,3 +22,24 @@ def parse_gemma(dataset : str, website_id : str) -> list[str]:
 
     return parsed_responsive_line
 
+def parse_qwen_coder(dataset : str, website_id : str) -> list[str]:
+    parsed_responsive_line = []
+    with open(dataset, "r", encoding="utf-8") as f:
+        data = json.load(f)
+        
+    responsive = None
+
+    for site in data:
+        if site.get("website_id") == website_id:
+            responsive = site.get('responsive_explanation')
+
+    print("Before: ", responsive, "\n")
+    responsive_lines = re.findall("(@media.*\s*)| ([\.\#][^!}]*[!};])| !([^!]+)!| \.(.*)!", responsive)
+    for line in responsive_lines:
+        parsed_line = next((x for x in line if x), None)
+        
+        if parsed_line:
+            parsed_line = parsed_line.strip('! `').strip()
+            print(f"Responsive_line: {parsed_line}")
+            parsed_responsive_line.append(parsed_line)
+    return parsed_responsive_line
